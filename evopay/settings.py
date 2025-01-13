9b3a3f1987538ac5bd32a8721bea4c8fa37f93bb
Ermal <ermalhx@gmail.com>
2025-01-13 20:29:14 +0100

    dsfsdfgsdg

--- evopay/settings.py
+++ evopay/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY') #'django-insecure-at7pz^)_^cxt1+7o906j$49qxfnjq((yrx2!btdwfkz36)=a@z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','https://web-production-f8dbd.up.railway.app/'
                  ]
